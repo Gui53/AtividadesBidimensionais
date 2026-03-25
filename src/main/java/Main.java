ddd0ed7703df89a535c3975f02033b4d0b01f13c
Gui53 <gui53tube@gmail.com>
2026-03-25 11:46:02 -0300

    Feat: criação do array bidimensional 3x3

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,6 +2,10 @@
 public class Main {
 
     public static void main(String[] args) {
-        
+        int[][] bidimensao = {
+            {1, 5, 6},
+            {8, 2, 3},
+            {7, 9, 4}
+        };
     }
 }
