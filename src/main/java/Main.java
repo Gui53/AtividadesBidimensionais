fea2252338febdfa07a0ce7b31b21081516646cc
Gui53 <gui53tube@gmail.com>
2026-03-26 18:28:25 -0300

    Feat: Soma de cada coluna do array bidimensional.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,21 +1,43 @@
-
 import javax.swing.JOptionPane;
 
 public class Main {
 
     public static void main(String[] args) {
         int[][] bidimensao = {
-            {1, 5, 6}, 
+            {1, 5, 6},
             {8, 3, 2},
             {7, 9, 4}};
-        
+
         int coluna = 0;
         int somaColuna = 0;
         
+        //Soma primeira linha
         for (int i = 0; i < bidimensao.length; i++) {
             somaColuna += bidimensao[i][coluna];
         }
-        
+
         JOptionPane.showMessageDialog(null, "Soma da primeira coluna: " + somaColuna);
+         
+        
+        //Soma cada linha
+        for (int i = 0; i < bidimensao.length; i++) {
+            somaColuna += bidimensao[i][coluna];
+        }
+        JOptionPane.showMessageDialog(null, "Soma da primeira coluna: " + somaColuna);
+        somaColuna = 0;
+        coluna += 1;
+
+        for (int i = 0; i < bidimensao.length; i++) {
+            somaColuna += bidimensao[i][coluna];
+        }
+        JOptionPane.showMessageDialog(null, "Soma da segunda coluna: " + somaColuna);
+        somaColuna = 0;
+        coluna += 1;
+
+        for (int i = 0; i < bidimensao.length; i++) {
+            somaColuna += bidimensao[i][coluna];
+        }
+        JOptionPane.showMessageDialog(null, "Soma da terceira coluna: " + somaColuna);
+
     }
 }
