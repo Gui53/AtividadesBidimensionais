b87f868241c7d1ffe48c8268d3635df6a770641a
Gui53 <gui53tube@gmail.com>
2026-03-26 14:16:52 -0300

    Feat: Soma da primeira coluna do array bidimensional.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,21 @@
 
+import javax.swing.JOptionPane;
+
 public class Main {
 
     public static void main(String[] args) {
         int[][] bidimensao = {
-            {1, 5, 6},
-            {8, 2, 3},
-            {7, 9, 4}
-        };
+            {1, 5, 6}, 
+            {8, 3, 2},
+            {7, 9, 4}};
+        
+        int coluna = 0;
+        int somaColuna = 0;
+        
+        for (int i = 0; i < bidimensao.length; i++) {
+            somaColuna += bidimensao[i][coluna];
+        }
+        
+        JOptionPane.showMessageDialog(null, "Soma da primeira coluna: " + somaColuna);
     }
 }
