5d691b37d678160a68f2c659133f9b71ae320e27
Gui53 <gui53tube@gmail.com>
2026-03-27 10:58:21 -0300

    feat: Soma de todos os elementos

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -12,14 +12,14 @@ public class Main {
         int coluna = 0;
         int somaColuna = 0;
         String resultado = "";
-        
+
         //Soma primeira coluna
         for (int i = 0; i < bidimensao.length; i++) {
             somaColuna += bidimensao[i][coluna];
         }
 
         JOptionPane.showMessageDialog(null, "Soma da primeira coluna: " + somaColuna);
-         
+        somaColuna = 0;
 
         //Soma cada coluna
         for (int j = 0; j < bidimensao[0].length; j++) {
@@ -32,14 +32,25 @@ public class Main {
 
         }
         JOptionPane.showMessageDialog(null, resultado);
-        
+
         //Multiplicação da primeira linha
-        int produto = 1,linha = 0;
-        
-        for(int j = 0; j < bidimensao[linha].length; j++){
+        int produto = 1, linha = 0;
+
+        for (int j = 0; j < bidimensao[linha].length; j++) {
             produto *= bidimensao[linha][j];
         }
-        
+
         JOptionPane.showMessageDialog(null, "Produto da primeira linha: " + produto);
+
+        //Soma de todos os elementos 
+        int somaTotal = 0;
+
+        for (int i = 0; i < bidimensao[0].length; i++) {
+            for (int j = 0; j < bidimensao.length; j++) {
+                somaTotal += bidimensao[i][j];
+            }
+        }
+        JOptionPane.showMessageDialog(null, "Soma total dos elementos: " + somaTotal);
+
     }
 }
